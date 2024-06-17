b25e5cd6ae41e677026b2c29cbab5f49a702b624
DESKTOP-11KO03I\student <231075@st.yoshida-g.ac.jp>
2024-06-17 10:32:12 +0900

    不要なファイルを削除

--- app/src/routes/web.php
+++ app/src/routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\PlayerItemController;
+use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+#ルート(ログイン画面)
+Route::get('/', [LoginController::class, 'index']);
+
+#ログイン処理
+Route::post('accounts/dologin', [LoginController::class, 'dologin']);
+
+#ログアウト処理
+Route::post('accounts/dologout', [LoginController::class, 'dologout']);
+
+#ホーム画面
+Route::get('accounts/home', [HomeController::class, 'index']);
+
+#アカウント一覧画面
+Route::get('accounts/index/{account_id?}', [AccountController::class, 'index']);
+
+#プレイヤー一覧画面
+Route::get('accounts/playerList', [PlayerController::class, 'index']);
+
+#アイテム一覧画面
+Route::get('accounts/itemList', [ItemController::class, 'index']);
+
+#所持アイテム一覧画面
+Route::get('accounts/playerItemList', [PlayerItemController::class, 'index']);
