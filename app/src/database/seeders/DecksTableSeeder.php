3550bcd00eb1ba2a1b22966c7ae031fa0c064c13
DESKTOP-11KO03I\student <231075@st.yoshida-g.ac.jp>
2024-10-07 13:49:24 +0900

    シードを制御

--- app/src/database/seeders/DecksTableSeeder.php
+++ app/src/database/seeders/DecksTableSeeder.php
@@ -9,34 +9,6 @@ class DecksTableSeeder extends Seeder
 {
     public function run(): void
     {
-        Deck::create([
-            'user_id' => 3,
-            'card_id' => 1,
-        ]);
 
-        Deck::create([
-            'user_id' => 3,
-            'card_id' => 3,
-        ]);
-
-        Deck::create([
-            'user_id' => 3,
-            'card_id' => 2,
-        ]);
-
-        Deck::create([
-            'user_id' => 3,
-            'card_id' => 1,
-        ]);
-
-        Deck::create([
-            'user_id' => 42,
-            'card_id' => 4,
-        ]);
-
-        Deck::create([
-            'user_id' => 42,
-            'card_id' => 5,
-        ]);
     }
 }
