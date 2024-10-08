d3411a29c188805611e7e73f548393de80e0b0d7
DESKTOP-11KO03I\student <231075@st.yoshida-g.ac.jp>
2024-10-08 10:44:59 +0900

    リソースの調整

--- api/src/app/Http/Resources/UsableCardResource.php
+++ api/src/app/Http/Resources/UsableCardResource.php
@@ -17,6 +17,7 @@ class UsableCardResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'type' => $this->type,
             'stack' => $this->stack,
             'created_at' => $this->created_at->format('Y/m/d/H:i:s'),
             'updated_at' => $this->updated_at->format('Y/m/d/H:i:s')
